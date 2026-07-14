9e246a25b075357a468b30aa39b57bb6797d488d
khaingyadanar696-byte <khaingyadanar696@gmail.com>
2026-07-14 10:48:01 +0630

    bus menu

---
+++ backend/src/main/java/com/cbk/trip/controller/BusController.java
@@ -0,0 +1,86 @@
+package com.cbk.trip.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cbk.trip.dto.BusDTO;
+import com.cbk.trip.dto.PageableDTO;
+import com.cbk.trip.enums.Status;
+import com.cbk.trip.service.BusService;
+import com.cbk.trip.utils.CommonUtil;
+
+/**
+ * @author HtetAungThan
+ * @since 11/Jan/2025
+ */
+@RestController
+@RequestMapping("/api/auth/bus")
+public class BusController {
+
+	@Autowired
+	BusService busService;
+
+	@PreAuthorize("hasAnyAuthority('ADMIN','CUSTOMER')")
+	@GetMapping
+	public ResponseEntity<?> getBuses(@Param("name") String name, @Param("status") Status status,
+			@PageableDefault(size = Integer.MAX_VALUE, sort = "updatedDate") Pageable pageable) {
+
+		PageableDTO busDTO = busService.getBuses(name, status, pageable);
+		return new ResponseEntity<>(busDTO, HttpStatus.OK);
+	}
+
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@PostMapping
+	public ResponseEntity<?> register(@Valid @RequestBody BusDTO busDTO, Errors errors) {
+
+		if (busService.isNameDuplicate(busDTO.getName(), null)) {
+			errors.rejectValue("name", "error.name", "Name already duplicated");
+		}
+
+		if (errors.hasErrors()) {
+			return CommonUtil.getFieldErrors(errors);
+		}
+
+		BusDTO respondDTO = busService.save(busDTO, false);
+		return new ResponseEntity<>(respondDTO, HttpStatus.CREATED);
+	}
+
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@PutMapping
+	public ResponseEntity<?> update(@Valid @RequestBody BusDTO busDTO, Errors errors) {
+
+		if (busService.isNameDuplicate(busDTO.getName(), busDTO.getId())) {
+			errors.rejectValue("name", "error.name", "Name already duplicated");
+		}
+
+		if (errors.hasErrors()) {
+			return CommonUtil.getFieldErrors(errors);
+		}
+
+		busService.save(busDTO, true);
+		return new ResponseEntity<>(CommonUtil.responseSuccessMessage("Bus updated"), HttpStatus.OK);
+	}
+	@PreAuthorize("hasAnyAuthority('ADMIN','CUSTOMER')")
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getById(@PathVariable(required = true, name = "id") Long id) {
+
+		BusDTO busDTO = busService.getById(id);
+
+		return new ResponseEntity<>(busDTO, HttpStatus.OK);
+	}
+}
---
+++ backend/src/main/java/com/cbk/trip/service/BusService.java
@@ -0,0 +1,64 @@
+package com.cbk.trip.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cbk.trip.dto.BusDTO;
+import com.cbk.trip.dto.PageableDTO;
+import com.cbk.trip.entity.Bus;
+import com.cbk.trip.enums.Status;
+import com.cbk.trip.repository.BusRepository;
+import com.cbk.trip.specification.BusSpecs;
+import com.cbk.trip.utils.CommonUtil;
+
+/**
+ * @author HtetAungThan
+ * @since 11/Jan/2025
+ */
+@Service
+public class BusService {
+	
+	@Autowired
+	BusRepository busRepository;
+
+	public boolean isNameDuplicate(String name, Long id) {
+		Optional<Bus> bus = busRepository.findByNameAndIdNot(name, id != null ? id : -1L);
+		return bus.isPresent();
+	}
+
+	public PageableDTO getBuses(String name, Status status, Pageable pageable) {
+		Page<Bus> page = busRepository.findAll(BusSpecs.getByFilter(name, status), pageable);
+		List<BusDTO> busDTOList = page.getContent().stream().map(BusDTO::new).collect(Collectors.toList());
+		return new PageableDTO(busDTOList, page);
+	}
+
+	@Transactional(rollbackFor = Exception.class)
+	public BusDTO save(@Valid BusDTO busDTO, boolean isUpdate) {
+		Bus bus = isUpdate ? CommonUtil.checkValidById(busDTO.getId(), busRepository) : new Bus();
+		
+		bus.setName(busDTO.getName());
+		bus.setStatus(busDTO.getStatus());
+		
+		return new BusDTO(busRepository.save(bus));
+	}
+
+	public BusDTO getById(Long id) {
+		Bus bus = CommonUtil.checkValidById(id, busRepository);
+		return new BusDTO(bus);
+	}
+
+	public List<BusDTO> getBusByStatus(Status status) {
+		Optional<List<Bus>> busListOpt = busRepository.findByStatus(status);
+		return busListOpt.map(list -> list.stream().map(BusDTO::new).collect(Collectors.toList()))
+				         .orElse(null);
+	}
+}
