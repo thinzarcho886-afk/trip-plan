bdc24168e998a9682be9288c99d8a52634268fde
Thinzar Soe <thinzarcho886@gmail.com>
2026-07-23 16:50:15 +0630

    chore:bookingSpecs

--- backend/src/main/java/com/cbk/trip/specification/BookingSpecs.java
+++ backend/src/main/java/com/cbk/trip/specification/BookingSpecs.java
@@ -15,8 +15,10 @@ import org.springframework.util.StringUtils;
 
 import com.cbk.trip.entity.Booking;
 import com.cbk.trip.entity.Booking_;
+import com.cbk.trip.entity.Customer;
 import com.cbk.trip.entity.Destination;
 import com.cbk.trip.entity.Package;
+import com.cbk.trip.entity.PaymentMethod;
 import com.cbk.trip.enums.Status;
 
 public class BookingSpecs {
@@ -32,25 +34,39 @@ public class BookingSpecs {
                 if (!Objects.isNull(packageId)) {
                     final Predicate packagePredicate = criteriaBuilder.equal(root.get(Booking_.PKG).get("id"), packageId);
                     predicates.add(packagePredicate);
-                    if (!StringUtils.isEmpty(packageName)) {
-    					Join<Booking, Package> bookingPackageJoin = root.join("pkg");
-    					Predicate bookingPredicate = criteriaBuilder.like(
-    							criteriaBuilder.lower(bookingPackageJoin.get("name")),
-    							"%" + packageName.toLowerCase() + "%"
-    					);
-    					predicates.add(bookingPredicate);
     				}
-                    }
+                if (!StringUtils.isEmpty(packageName)) {
+					Join<Booking, Package> bookingPackageJoin = root.join("pkg");
+					Predicate bookingPredicate = criteriaBuilder.like(
+							criteriaBuilder.lower(bookingPackageJoin.get("name")),
+							"%" + packageName.toLowerCase() + "%"
+					);
+					predicates.add(bookingPredicate);
+				}
                 if (!Objects.isNull(customerId)) {
                     final Predicate customerPredicate = criteriaBuilder.equal(root.get(Booking_.CUSTOMER).get("id"), customerId);
                     predicates.add(customerPredicate);
                 }
-
+                if (!StringUtils.isEmpty(customerName)) {
+					Join<Booking, Customer> bookingCustomerJoin = root.join("customer");
+					Predicate bookingPredicate = criteriaBuilder.like(
+							criteriaBuilder.lower(bookingCustomerJoin.get("name")),
+							"%" + customerName.toLowerCase() + "%"
+					);
+					predicates.add(bookingPredicate);
+				}
                 if (!Objects.isNull(paymentMethodId)) {
                     final Predicate paymentMethodPredicate = criteriaBuilder.equal(root.get(Booking_.PAYMENT_METHOD).get("id"), paymentMethodId);
                     predicates.add(paymentMethodPredicate);
                 }
-
+                if (!StringUtils.isEmpty(paymentMethodName)) {
+					Join<Booking, PaymentMethod> bookingPaymentJoin = root.join("paymentMethod");
+					Predicate bookingPredicate = criteriaBuilder.like(
+							criteriaBuilder.lower(bookingPaymentJoin.get("name")),
+							"%" + paymentMethodName.toLowerCase() + "%"
+					);
+					predicates.add(bookingPredicate);
+				}
                 if (!Objects.isNull(status)) {
                     final Predicate statusPredicate = criteriaBuilder.equal(root.get(Booking_.STATUS), status);
                     predicates.add(statusPredicate);
